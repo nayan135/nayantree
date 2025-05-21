77b1d7c6cb607ae35eca3bae7475d9a660099c6d
NAYAN ACHARYA <nayan374@proton.me>
2025-05-21 17:36:56 +0545

    something neww

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,10 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nayannow.xyz'
-  
   return [
     {
-      url: baseUrl,
+      url: 'https://nayannow.xyz',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
     },
-  
   ]
 }
